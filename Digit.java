74a733a1a4242f1e55f13ae7aed21d3ed7e741e9
ANITHAS22 <39801902+ANITHAS22@users.noreply.github.com>
2018-06-18 21:39:53 +0530

    Digit.java

---
+++ Digit.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Digit
+{
+    public static void main(String args[])
+    {
+        Scanner io=new Scanner(System.in);
+        int n=io.nextInt();
+        String a=String.valueOf(n);
+        System.out.println(a.length());
+    }
+}
